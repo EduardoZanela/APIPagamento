16b09a2238289b011747da71e0f3b477f66a9a31
Kelvin <106881@upf.br>
2016-10-21 17:12:10 -0200

    [misc] Fix indentation stripe

--- controller/stripe.php
+++ controller/stripe.php
@@ -1,30 +1,38 @@
 <?php
-use Omnipay\Omnipay;
+    use Omnipay\Omnipay;
 
-$gateway = Omnipay::create('Stripe');
-$gateway->setApiKey('abc123');
+    $gateway = Omnipay::create('Stripe');
+    $gateway->setApiKey('abc123');
 
-$price = floatval ($_SESSION['price']);
-$_SESSION['valorComprado'] = $price;
-$_SESSION['pagamento'] = 'cartaoCreditoStripe';
-
-
-$formData = array('number' => '4242424242424242', 'expiryMonth' => '6', 'expiryYear' => '2016', 'cvv' => '123');
-$response = $gateway->purchase(array('amount' => $price, 'currency' => 'BRL', 'card' => $formData))->send();
-
-if ($response->isSuccessful()) {
-    // payment was successful: update database
-    //print_r($response);
-    $_SESSION['id'] = $response->id;
-    include '../controller/orderRegister.php';
-    header("location:../view/complete.php");
-} elseif ($response->isRedirect()) {
-    // redirect to offsite payment gateway
-    $response->redirect();
-} else {
-    // payment failed: display message to customer
-    echo $response->getMessage();
-}
+    $price = floatval ($_SESSION['price']);
+    $_SESSION['valorComprado'] = $price;
+    $_SESSION['pagamento'] = 'cartaoCreditoStripe';
 
 
- ?>
+    $formData = array(
+        'number' => '4242424242424242',
+        'expiryMonth' => '6',
+        'expiryYear' => '2016',
+        'cvv' => '123'
+    );
+
+    $response = $gateway->purchase(array(
+        'amount' => $price,
+        'currency' => 'BRL',
+        'card' => $formData
+    ))->send();
+
+    if ($response->isSuccessful()) {
+        // payment was successful: update database
+        //print_r($response);
+        $_SESSION['id'] = $response->id;
+        include '../controller/orderRegister.php';
+        header("location:../view/complete.php");
+    } elseif ($response->isRedirect()) {
+        // redirect to offsite payment gateway
+        $response->redirect();
+    } else {
+        // payment failed: display message to customer
+        echo $response->getMessage();
+    }
+?>
