2c5a8fa402fb35b34c8d13283058f1a10ca51dbf
Kelvin <106881@upf.br>
2016-10-21 16:54:43 -0200

    [misc] Fix indentation cartao

--- view/cartao.php
+++ view/cartao.php
@@ -1,12 +1,11 @@
 <?php
-session_start();
+    session_start();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -27,97 +26,91 @@ session_start();
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
 </head>
 
 <body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">Integração API Pagamento</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">Sobre</a>
+                    </li>
+                    <li>
+                        <a href="#">Produtos</a>
+                    </li>
+                    <li>
+                        <a href="#">Contato</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
 
-<!-- Navigation -->
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <!-- Page Content -->
     <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="index.php">Integração API Pagamento</a>
+        <!-- Jumbotron Header -->
+        <header class="jumbotron hero-spacer">
+            <h1><?php echo $_SESSION['name'] ; echo $_SESSION['maxid'] ;?></h1>
+            <p><?php echo $_SESSION['description']?></p>
+        </header>
+
+        <hr>
+
+        <div class="container" style="width: 500px; float: left;">
+            <h2>Dados do Cartão de Credito</h2>
+            <form>
+                <div class="form-group">
+                    <label for="numero">Numero:</label>
+                    <input type="text" class="form-control" id="numero" placeholder="Numero do Cartão">
+                </div>
+                <div class="form-group">
+                    <label for="mes">Mes de Expiração:</label>
+                    <input type="text" class="form-control" id="mes" placeholder="Mes de Expiração">
+                </div>
+                <div class="form-group">
+                    <label for="mes">Ano de Expiração:</label>
+                    <input type="text" class="form-control" id="mes" placeholder="Ano de Expiração">
+                </div>
+                <div class="form-group">
+                    <label for="cvv">Codigo de Segurança:</label>
+                    <input type="text" class="form-control" id="cvv" placeholder="Codigo de Segurança">
+                </div>
+                <p>
+                    <a href="../controller/stripe.php" class="btn btn-primary">PAGAR</a>
+                </p>
+            </form>
         </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li>
-                    <a href="#">Sobre</a>
-                </li>
-                <li>
-                    <a href="#">Produtos</a>
-                </li>
-                <li>
-                    <a href="#">Contato</a>
-                </li>
-            </ul>
-        </div>
-        <!-- /.navbar-collapse -->
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2014</p>
+                </div>
+            </div>
+        </footer>
     </div>
     <!-- /.container -->
-</nav>
 
-<!-- Page Content -->
-<div class="container">
+    <!-- jQuery -->
+    <script src="../include/assets/js/jquery.js"></script>
 
-  <!-- Jumbotron Header -->
-  <header class="jumbotron hero-spacer">
-      <h1><?php echo $_SESSION['name'] ; echo $_SESSION['maxid'] ;?></h1>
-      <p><?php echo $_SESSION['description']?></p>
-  </header>
-
-    <hr>
-
-    <div class="container" style="width: 500px; float: left;">
-    <h2>Dados do Cartão de Credito</h2>
-    <form>
-      <div class="form-group">
-        <label for="numero">Numero:</label>
-        <input type="text" class="form-control" id="numero" placeholder="Numero do Cartão">
-      </div>
-      <div class="form-group">
-        <label for="mes">Mes de Expiração:</label>
-        <input type="text" class="form-control" id="mes" placeholder="Mes de Expiração">
-      </div>
-      <div class="form-group">
-        <label for="mes">Ano de Expiração:</label>
-        <input type="text" class="form-control" id="mes" placeholder="Ano de Expiração">
-      </div>
-      <div class="form-group">
-        <label for="cvv">Codigo de Segurança:</label>
-        <input type="text" class="form-control" id="cvv" placeholder="Codigo de Segurança">
-      </div>
-      <p>
-          <a href="../controller/checkoutPaypal.php" class="btn btn-primary">PAGAR</a>
-      </p>
-    </form>
-  </div>
-    
-    <!-- Footer -->
-    <footer>
-        <div class="row">
-            <div class="col-lg-12">
-                <p>Copyright &copy; Your Website 2014</p>
-            </div>
-        </div>
-    </footer>
-
-</div>
-<!-- /.container -->
-
-<!-- jQuery -->
-<script src="../include/assets/js/jquery.js"></script>
-
-<!-- Bootstrap Core JavaScript -->
-<script src="../include/assets/js/bootstrap.min.js"></script>
-
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../include/assets/js/bootstrap.min.js"></script>
 </body>
-
 </html>
